4ac616b3ab54d6d2a5f665d0314ea537fe037841
Your Name <evachamorro@Air-de-Eva-3.home>
2020-05-06 12:28:14 +0200

    clean

--- a_example_spectro.py
+++ a_example_spectro.py
@@ -1,24 +1,25 @@
+# Warping tutorial
+## A_example_spectro
 
-## Warping tutorial
-###Julien Bonnel-Eva Chamorro
-##April 2020
-
-##THIS CODE IS TO LOCALIZE FM SOURCES
+#### April 2020
+#### Eva Chamorro Garrido
 
 
-#import packages
+### Import packages
+import os
+import matplotlib
+matplotlib.use('TkAgg')
+#% matplotlib inline ## activate in jupyter notebook
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft
 import scipy.io as sio
-import os as os
-from time import sleep
-from tfrstft import tfrstft
+from time_frequency_analysis_functions import *
 
+### Load simulated signal
+data = sio.loadmat(os.getcwd() + '/sig_pek_for_warp.mat')
+s_t = data['s_t']
+fs = data['fs']
 
-## Load simulated signal
-data = sio.loadmat(os.getcwd()+ '/sig_pek_for_warp.mat')
-#print(data.keys())
 '''
 # s_t: propagated modes in a Pekeris waveguide with parameters
 #      c1, c2, rho1, rho2: sound speed / density
@@ -27,71 +28,63 @@ data = sio.loadmat(os.getcwd()+ '/sig_pek_for_warp.mat')
 #      zs, zr: source/receiver depth
 # s_t_dec: same than s_t, except that time origin has been set for warping
 # fs: sampling frequency
-'''
-s_t=data['s_t']
-fs=data['fs']
 
-'''
 ### NB: one can run optional_create_simulated_signal.m to generate another
 ### simulated signal
 ### IF YOU CHANGE RANGE, you must change the following variable which
 ### defines the bounds on the x-axis for all the plots
 '''
-xlim_plots=[6.5,7.5]
 
-## Compute time-frequency representation
+xlim_plots = [6.5, 7.5]
+
+### Compute time-frequency representation
 
 # Signal length
-N=np.size(s_t)
+N = np.size(s_t)
 
 # Short Time Fourier Transform parameters
-vec_t=np.arange(1,N+1); vec_t = vec_t[np.newaxis,:] # time samples where the STFT will be computed (i.e. positions of the sliding window)
+vec_t = np.arange(1, N + 1)
 
-NFFT=2048 # FFT size
-N_window=31 # sliding window size (need an odd number)
+vec_t = vec_t[np.newaxis, :]  # time samples where the STFT will be computed (i.e. positions of the sliding window)
 
+NFFT = 2048  # FFT size
+N_window = 31  # sliding window size (need an odd number)
 
-#STFT computation
-
-tfr,t,f=tfrstft(s_t,vec_t,NFFT,N_window)
+# STFT computation
+d = np.hamming(N_window)
+d = d[:, np.newaxis]
+tfr = tfrstft(s_t, vec_t, NFFT, d)
 
 # Spectrogram ~ modulus STFT
-spectro=abs(tfr)**2
+spectro = abs(tfr) ** 2
 
 # Time and frequency axis
-time=np.arange(0,N)/fs
-freq=np.arange(0,NFFT)*fs/NFFT
+time = np.arange(0, N) / fs
+freq = np.arange(0, NFFT) * fs / NFFT
 
+### Plots
 
-
-plt.figure(figsize=(9.0,7.0))
-plt.plot(time[0,:], s_t[0,:])
+plt.figure(figsize=(9.0, 7.0))
+plt.plot(time[0, :], s_t[0, :])
 plt.xlim(xlim_plots)
 plt.xlabel('Time (sec)')
 plt.ylabel('Amplitude (A.U.)')
 plt.title('Signal in the time domain')
 plt.grid()
+plt.show()
 
 print('The code has loaded a signal propagated in a Pekeris waveguide')
 print('and shows the time series')
 input('Press ENTER to continue and see the spectrogram')
 
-
-plt.pause(0.5)
-
-plt.close('all')
-
-plt.figure(figsize=(9.0,7.0))
-
-
-plt.pcolormesh(time[0,:],freq[0,:],spectro)
+plt.figure(figsize=(9.0, 7.0))
+plt.pcolormesh(time[0, :], freq[0, :], spectro)
 plt.xlim(xlim_plots)
-plt.ylim([0,fs/2])
-
+plt.ylim([0, fs / 2])
 plt.xlabel('Time (sec)')
 plt.ylabel('Frequency (Hz)')
 plt.title('Spectrogram')
-
+plt.show()
 
 print('This is the spectrogram, computed with a sliding window ')
 print('of length Nw=31 samples (or 0.15 s , since the sampling frequency is 200 Hz')
@@ -100,24 +93,16 @@ print('segments of length Nw. Another important parameter for spectrogram comput
 print('overlap. In this tutorial, the overlap between segment is Nw-1 samples, which is ')
 print('the maximum overlap that one can obtain : the percentage of overlap is 100*(Nw-1)/Nw')
 print('Such a high overlap will be handy for modal filtering')
-input('Press ENTER to continue...')
-plt.pause(0.5)
-
+input('Press ENTER to continue')
 
 print('Now you can modify the window length')
 print('Remember to give it in number of samples')
 print('Number between 11 and 71 are reasonable tries')
 print('(for coding reason, please use odd numbers only)')
 
-
-##This part is not working well only works one time
-# 'Continue' is not working I think is a code style parameter of PyCharm
-# I have to investigate because in Jupyter is working well
-
 while ((N_window != 999)):
     print('Input window length and press enter for spectrogram computation : ')
     N_window = int(input('(window length 0 to exit the code) '))
-    sleep(30)
 
     if ((N_window == 0) or (N_window == 999)):
         print('  ')
@@ -126,20 +111,16 @@ while ((N_window != 999)):
 
     if ((N_window != 999)):
         # STFT computation
-        tfr,t,f = tfrstft(s_t, vec_t, NFFT, N_window)
+        d = np.hamming(N_window)
+        d = d[:, np.newaxis]
+        tfr = tfrstft(s_t, vec_t, NFFT, d)
         # Spectrogram ~ modulus STFT
         spectro = abs(tfr) ** 2
-        plt.figure(figsize=(9.0,7.0))
+        plt.figure(figsize=(9.0, 7.0))
         plt.pcolormesh(time[0, :], freq[0, :], spectro)
         plt.ylim([0, fs / 2])
         plt.xlim(xlim_plots)
         plt.xlabel('Time (sec)')
         plt.ylabel('Frequency (Hz)')
-        plt.title('Spectrogram N_window: '+ str(N_window))
+        plt.title('Spectrogram N_window: ' + str(N_window))
         plt.show()
-        
-
-
-
-
-
